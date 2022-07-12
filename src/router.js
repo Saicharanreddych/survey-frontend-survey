1922d8eebe4322fce93b4d258825c4d5b9bac6d9
Sanjay <Sanjay.thipiri@eagles.oc.edu>
2022-07-12 17:07:56 -0500

    Provided Register support

--- src/router.js
+++ src/router.js
@@ -1,9 +1,8 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue";
-import Login from "./components/Login.vue";
 import Register from "./components/Register.vue";
 // lazy-loaded
-const Profile = () => import("./components/Profile.vue")
+
 //const BoardAdmin = () => import("./components/BoardAdmin.vue")
 //const BoardModerator = () => import("./components/BoardModerator.vue")
 //const BoardUser = () => import("./components/BoardUser.vue")
@@ -18,20 +17,12 @@ const routes = [
     path: "/home",
     component: Home,
   },
-  {
-    path: "/login",
-    component: Login,
-  },
+ 
   {
     path: "/register",
     component: Register,
   },
-  {
-    path: "/profile",
-    name: "profile",
-    // lazy-loaded
-    component: Profile,
-  },
+
   
 ];
 
