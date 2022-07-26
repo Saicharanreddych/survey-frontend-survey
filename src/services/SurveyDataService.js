02eba5f93b214b6bb354393de05b205b02c3954a
saicharanreddych <reddy.chintakuntla@eagles.oc.edu>
2022-07-26 02:08:58 -0500

    Provided add survey support in front end.

--- src/services/SurveyDataService.js
+++ src/services/SurveyDataService.js
@@ -1,13 +1,20 @@
+
 import http from "../http-common";
 class SurveyDataService{
     create(data)
     {
         return http.post("/surveys/createsurvey",data);
     }
+    insertquestions(data,surveyid)
+    {
+        return http.post(`/surveys/insert/${surveyid}`,data);
+    }
+    
     getAll()
     {
         return http.get("/surveys/getsurveys");
     }
+    
     findByName(name) {
         return http.get(`/surveys/getsurveys?name=${name}`);
       }
