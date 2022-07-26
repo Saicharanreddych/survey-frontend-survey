dd3da1781a9fc2b40f1584342bf049726df1e019
Nikhil <nikhilreddy.gaddam@eagles.oc.edu>
2022-07-26 02:29:08 -0500

    Provided survey view support in frontend.

--- src/services/SurveyDataService.js
+++ src/services/SurveyDataService.js
@@ -9,12 +9,18 @@ class SurveyDataService{
     {
         return http.post(`/surveys/insert/${surveyid}`,data);
     }
-    
+    get(id) {
+        return http.get(`/surveys/${id}`);
+      }
+    getAllQuestions(id) {
+        return http.get(`/surveys/questions/${id}`);
+      }
     getAll()
     {
         return http.get("/surveys/getsurveys");
     }
-    
+   
+
     findByName(name) {
         return http.get(`/surveys/getsurveys?name=${name}`);
       }
